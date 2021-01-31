9392a68654eba755ea1c532f514adbb7051dc425
Łukasz Madera <madera.lukasz@gmail.com>
2021-01-31 17:33:24 +0100

    init_commit

---
+++ cypress/integration/sample3.spec.js
@@ -0,0 +1,9 @@
+/// <reference types="Cypress" />
+
+context('Second sample test', () => {
+  
+    it('A passing test', () => {
+        cy.document().title().should('not.contain', 'unicorns')
+    })
+
+  })
